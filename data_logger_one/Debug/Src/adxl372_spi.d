9624a10ce081de14abe2407e716e470375810010
Takumi Otsuka <takumi.o.0410@gmail.com>
2020-02-03 23:32:07 +0900

    add debug files

---
+++ data_logger_one/Debug/Src/adxl372_spi.d
@@ -0,0 +1,22 @@
+Src/adxl372_spi.o: ../Src/adxl372_spi.c \
+ C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/error.h \
+ C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/adxl372.h \
+ C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/util.h \
+ C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/delay.h \
+ C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/gpio.h \
+ C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/i2c.h \
+ C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/spi.h
+
+C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/error.h:
+
+C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/adxl372.h:
+
+C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/util.h:
+
+C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/delay.h:
+
+C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/gpio.h:
+
+C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/i2c.h:
+
+C:/Users/takum/Documents/GitHub/Data_Logger_One/data_logger_one/Inc/spi.h:

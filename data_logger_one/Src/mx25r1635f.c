8063132b09bb49b460f60c129c7faf7ccec4aaa2
Takumi Otsuka <takumi.o.0410@gmail.com>
2020-03-09 23:32:36 +0900

    エラー部分をコメントアウト

--- data_logger_one/Src/mx25r1635f.c
+++ data_logger_one/Src/mx25r1635f.c
@@ -73,6 +73,7 @@ uint8_t MX25qxx_ReadStatusRegister(uint8_t SelectStatusRegister_1_2_3)
 }
 
 //###################################################################################################################
+/*
 void MX25Rxx_WaitForWriteEnd(void)
 {
 	uint8_t status = 0;
@@ -88,11 +89,11 @@ void MX25Rxx_WaitForWriteEnd(void)
 	while ((mx25rxx.StatusRegister1 & 0x01) == 0x01);
 	MX25_CS_HIGH();
 }
-
+*/
 //###################################################################################################################
 void MX25Rxx_WriteByte(uint8_t pBuffer, uint32_t WriteAddr_inBytes)
 {
-	MX25Rxx_WaitForWriteEnd();
+	// MX25Rxx_WaitForWriteEnd();
 	MX25Rxx_WriteEnable();
 	MX25_CS_LOW();
 	MX25Rxx_Spi(FLASH_CMD_PP);
@@ -101,7 +102,7 @@ void MX25Rxx_WriteByte(uint8_t pBuffer, uint32_t WriteAddr_inBytes)
 	MX25Rxx_Spi(WriteAddr_inBytes & 0xFF);
 	MX25Rxx_Spi(pBuffer);
 	MX25_CS_HIGH();
-	MX25Rxx_WaitForWriteEnd();
+	// MX25Rxx_WaitForWriteEnd();
 }
 
 //###################################################################################################################
